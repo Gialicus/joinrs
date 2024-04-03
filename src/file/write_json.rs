338d6f1ea67f9b6be90d03a829da56c15cdb9e86
g.coletta <giali.coletta@fullstackagency.it>
2024-04-03 21:45:05 +0200

    Update write_json.rs

--- src/file/write_json.rs
+++ src/file/write_json.rs
@@ -5,7 +5,6 @@ use serde_json::{to_string_pretty, Value};
 
 pub fn write_json(path: PathBuf, data: Vec<Value>) -> Result<()> {
     let json_str = to_string_pretty(&data)?;
-    println!("{:?}", path);
     let mut file = File::create(path)?;
     file.write_all(json_str.as_bytes())?;
     Ok(())
